18dac5b717b0294616a936942d541e164f4b50d4
aidanstapleton <asta5077@uni.sydney.edu.au>
2022-04-27 21:50:10 +1000

    change topic and add main function

--- assignment_3/scripts/odomReadings.py
+++ assignment_3/scripts/odomReadings.py
@@ -2,24 +2,38 @@
 
 # Subscribe to the odom topic to get the turtlebot odometry readings  
 import rospy
-from std_msgs.msg import String
+from nav_msgs.msg import Odometry
     
 def callback(data):
+    print("Hello")
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-        
-def listener():
-    
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('odom_node', anonymous=True)
 
-    rospy.Subscriber("/odom", String, callback)
+def main():
+    try:
+        # Initialise a new node
+        rospy.init_node('odom_node')
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        # Change the spin rate to 1 Hz which is ~1 second
+        rate = rospy.Rate(1)
+
+        # Continuous loop while ROS is running
+        while not rospy.is_shutdown():
+
+            # Subscribe to published images and camera intrinsic parameters
+            rospy.Subscriber("/odom", Odometry, callback)
+            #rospy.Subscriber("/raspicam_node/camera_info", CameraInfo, cam_param_callback)
+            
+            #print("\n\n HERE IN MAIN\n\n")
+
+            # Sleep until next spin sduycbs
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        # exit if there is any interruption from ROS
+        return
+    except KeyboardInterrupt:
+        # exit if there is any interrupt from the keyboard (e.g. Ctrl+c)
+        return
    
 if __name__ == '__main__':
-    listener()
+    main()
