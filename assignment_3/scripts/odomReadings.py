b512837bb5fb74020faf703131d8bf206cd072f4
Antony Wu <antony6889@gmail.com>
2022-05-01 23:14:43 +1000

    Fixed scope issues with laser data variables, icp working

--- assignment_3/scripts/odomReadings.py
+++ assignment_3/scripts/odomReadings.py
@@ -3,7 +3,6 @@
 
 # Subscribe to the odom topic to get the turtlebot odometry readings  
 import rospy
-import math
 import copy
 import numpy as np
 import open3d as o3d
@@ -15,13 +14,6 @@ from sensor_msgs.msg import LaserScan           # /scan
 from tf.msg import tfMessage
 from scipy.spatial.transform import Rotation
 
-#################################################################################
-# Global variables
-tf_trans = list()
-
-# Open3d point clouds
-new_pcd = o3d.geometry.PointCloud()
-old_pcd = o3d.geometry.PointCloud()
 
 #################################################################################
 # Subscriber callback function for odom saves data
@@ -95,52 +87,43 @@ def error(lidarDataX, lidarDataY, odomDataX, odomDataY, pointInteval):
 # Subscriber callback function for the laser scan saves data
 def laser_callback(data):
 
+    global new_data, laser_ranges
+
     # Update old pointcloud variable - NEED TO COPY OR ELSE IS PYTHON REFERENCES
     old_pcd = copy.deepcopy(new_pcd)
 
-    # Global variables for odom data
-    global laser_ranges
-
     # Save the ranges and intensities from the odometry readings
-    laser_ranges = np.array([data.ranges], np.float64)
+    laser_ranges = np.array([data.ranges], np.float64)          # size: 1 x 360
 
-    # Empty row of zeroes for Vector3dVector - this seems to work better in calculating transformation
-    zero_row = np.zeros(laser_ranges.size)
+    # If the variable is currently empty, will need to set new_data first
+    if new_data.size < 360:
+        new_data = laser_ranges
 
-    # Create the n x 3 matrix for open3d type conversion
-    new_data = np.vstack([laser_ranges, zero_row, zero_row])
-    new_data = np.transpose(new_data)
+    # Check the data is not the same
+    elif new_data is not laser_ranges:
 
-    # Convert the numpy array to open3d type
-    new_pcd.points = o3d.utility.Vector3dVector(new_data)
+        # Add 1D LIDAR data array to the next available row
+        new_data = np.append(new_data, laser_ranges,axis=0)
 
-    # print(np.asarray(old_pcd.points))
-    # print(np.asarray(new_pcd.points))
+    # Test if the variable has 3 columns - means n x 3 is full yet
+    if new_data.size == 1080:
 
-    # Check that the pointclouds are different
-    if old_pcd.points != new_pcd.points and np.asarray(old_pcd.points).size != 0:
-        
-        # Conduct ICP registration
-        icp_registration(old_pcd, new_pcd)
+        # Transpose the matrix for open3d format input
+        new_data = np.transpose(new_data)                       # size: 360 x 3
+
+        # Convert the numpy array to open3d type
+        new_pcd.points = o3d.utility.Vector3dVector(new_data)
+
+        # Check that the pointclouds are different
+        if old_pcd.points != new_pcd.points and np.asarray(old_pcd.points).size != 0:
+            
+            # Conduct ICP registration
+            icp_registration(old_pcd, new_pcd)
+
+        # Clear the new_data variable for the next iteration of points
+        new_data = np.array([])
 
     # LATER IN Q1 CAN CHANGE THE MAX AND MIN RANGE BY WRITING TO RANGE_MIN AND RANGE_MAX
-
-#################################################################################
-# Subscriber callback function for tf saves data
-def tf_callback(data):
-
-    # Global variables for odom data
-    global tf_transform
-
-    # Save the data from the odometry readings
-    tf_transform = data.transforms
-
-    # print("\n\n-------------------------------")
-    print(tf_transform)
-
-    # Can see frame relationship between Odom and laser here
-    # IN TERMINAL run: rostopic echo tf --> to see the frames
-    #             run: rosrun tf tf_echo /frame1 /frame2 --> to see transform from frame 1 to frame 2
 
 #################################################################################
 # Convert quaternion into a rotation matrix
@@ -214,17 +197,21 @@ def icp_registration(source, target):
         source, target, threshold, init_trans,
         o3d.registration.TransformationEstimationPointToPoint(),
         o3d.registration.ICPConvergenceCriteria(max_iteration=10000))
-    # print(reg_p2p.transformation)     # type: numpy.ndarray
+    print(reg_p2p.transformation)     # type: numpy.ndarray
     # draw_registration_result(source, target, reg_p2p.transformation)
 
 #################################################################################
 def main():
 
-    # Global x and y pose list variables
-    global odomX
-    global odomY
+    # Global vairables
+    global odomX, odomY, new_data, new_pcd, old_pcd
     odomX = []
     odomY = []
+    new_data = np.array([])
+
+    # Open3d point clouds
+    new_pcd = o3d.geometry.PointCloud()
+    old_pcd = o3d.geometry.PointCloud()
 
     try:
         # Initialise a new node
@@ -233,6 +220,7 @@ def main():
         # Change the spin rate to 1 Hz which is ~1 second
         rate = rospy.Rate(1)
 
+        # Listener variable for tf topic
         listener = tf.TransformListener()
 
         # Continuous loop while ROS is running
@@ -243,7 +231,6 @@ def main():
             # Subscribe to odometry, scan topics
             # rospy.Subscriber("/odom", Odometry, odom_callback, queue_size=1)
             rospy.Subscriber("/scan", LaserScan, laser_callback, queue_size=1)
-            # rospy.Subscriber("/tf", tfMessage, tf_callback, queue_size=1)
 
             # Subscribe to the translation (linear transformation in x, y, z) 
             # and rotation (quaternion in x, y, z, w) of the child relative to parent frame
@@ -254,9 +241,6 @@ def main():
 
             # Get the transformation from /odom (world) to the LIDAR scanner frame
             transformation = get_transformation_matrix(tf_rot, tf_trans)
-            print(transformation)
-
-            # print(transformation)
                 
             # Sleep until next spin
             rate.sleep()
