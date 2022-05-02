95ab7f78cf7cae0af9403216288c489659b24c9a
Kieran Nolan <knol5284@uni.sydney.edu.au>
2022-05-03 04:06:19 +1000

    Uncomment SLAM
    Hope it works

--- Lab3_SLAM/Unit_Tests.py
+++ Lab3_SLAM/Unit_Tests.py
@@ -7,7 +7,7 @@ import geometry
 
 import random
 
-# from LandmarkSLAM_students import LeastSquaresSolver
+from LandmarkSLAM_students import LeastSquaresSolver
 
 # Covariance to experiment with
 pose_measurement_covariance = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])*10**(-2)
